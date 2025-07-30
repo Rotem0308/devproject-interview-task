d832f2ce18d633de273456c62f2a6119466dbd9c
Rotem0308 <105914305+Rotem0308@users.noreply.github.com>
2025-07-30 04:33:39 +0300

    update:  home page title size on small screen

--- frontend/next-frontend/app/page.tsx
+++ frontend/next-frontend/app/page.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const Home = () => {
   return (
     <div className="p-4 h-full flex flex-col items-center">
-      <div className="mb-20 font-bolder text-5xl font-manrope text-center w-[70%]">
+      <div className="mb-20 font-bolder text-4xl sm:text-5xl font-manrope text-center w-[70%]">
         <span>Turn your goals into action — </span>
-        <span className="main-app-text-grediant font-mono text-6xl font-bold">
+        <span className="main-app-text-grediant font-mono text-5xl sm:text-6xl font-bold">
           start mastering your tasks now.
         </span>
       </div>
